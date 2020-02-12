6092949771629846bed08f54c806ba23a866b0c4
Julia Pickett-Garcia <s199064@students.aie.edu>
2020-02-12 12:40:12 -0800

    NavMesh! All the exercises up to New Paths are done. Custom Locomotion is yet to be started.

---
+++ Assets/Scripts/NavigateToCursor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigateToCursor : MonoBehaviour
+{
+    private Camera cam;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                //Debug.Log("Hit!: " + hit.point.x + ", " + hit.point.y + ", " + hit.point.z);
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/YeetOnClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YeetOnClick : MonoBehaviour
+{
+    public GameObject toYeet;
+
+    private bool isActive = true;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isActive = !isActive;
+            toYeet.SetActive(!isActive);
+        }
+    }
+}
